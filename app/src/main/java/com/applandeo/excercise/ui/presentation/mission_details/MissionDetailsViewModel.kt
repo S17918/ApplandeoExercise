7fee5f20196e445076faef99cab75e36a6d9c31f
Piotr Wasilewski <61659817+S17918@users.noreply.github.com>
2021-07-28 18:25:25 +0200

    Update MissionDetailsViewModel.kt

--- app/src/main/java/com/applandeo/excercise/ui/presentation/mission_details/MissionDetailsViewModel.kt
+++ app/src/main/java/com/applandeo/excercise/ui/presentation/mission_details/MissionDetailsViewModel.kt
@@ -3,5 +3,5 @@ package com.applandeo.excercise.ui.presentation.mission_details
 import androidx.lifecycle.ViewModel
 
 class MissionDetailsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
 }
